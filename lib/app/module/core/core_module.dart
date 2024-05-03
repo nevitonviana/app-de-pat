ff286ecce825b227a619825dd73ab98dee343aab
Neviton Viana <nevitonviana1@hotmail.com>
2024-05-03 19:53:05 -0300

    Encapsulando logs da aplicação

--- lib/app/module/core/core_module.dart
+++ lib/app/module/core/core_module.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../core/logger/app_logger.dart';
+import '../../core/logger/logger_app_logger_impl.dart';
 import '../../core/rest_client/dio/dio_rest_client.dart';
 import '../../core/rest_client/rest_client.dart';
 import 'auth/auth_store.dart';
@@ -9,5 +11,6 @@ class CoreModule extends Module {
   void exportedBinds(i) {
     i.addLazySingleton(AuthStore.new);
     i.addLazySingleton<RestClient>(DioRestClient.new);
+    i.addLazySingleton<AppLogger>(LoggerAppLoggerImpl.new);
   }
 }
