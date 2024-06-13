fbc30ec4bfd09891e8c454625daa4d8114bd9a19
Neviton Viana <nevitonviana1@hotmail.com>
2024-06-13 15:22:29 -0300

    Conectando a tela e buscando os dados Parte 1

---
+++ lib/app/module/supplier/supplier_controller.dart
@@ -0,0 +1,70 @@
+import 'package:mobx/mobx.dart';
+
+import '../../core/life_cycle/controller_life_cycle.dart';
+import '../../core/logger/app_logger.dart';
+import '../../core/ui/widgets/loader.dart';
+import '../../core/ui/widgets/messages.dart';
+import '../../models/supplier_model.dart';
+import '../../models/supplier_services_model.dart';
+import '../../services/supplier/supplier_service.dart';
+
+part 'supplier_controller.g.dart';
+
+class SupplierController = SupplierControllerBase with _$SupplierController;
+
+abstract class SupplierControllerBase with Store, ControllerLifeCycle {
+  final SupplierService _supplierService;
+  final AppLogger _log;
+
+  SupplierControllerBase({
+    required SupplierService supplierService,
+    required AppLogger log,
+  })  : _supplierService = supplierService,
+        _log = log;
+
+  int _supplierId = 0;
+
+  @readonly
+  SupplierModel? _supplierModel;
+
+  @readonly
+  var _supplierServices = <SupplierServicesModel>[];
+
+  @override
+  void onInit([Map<String, dynamic>? params]) {
+    _supplierId = params?['supplierId'] ?? 0;
+  }
+
+  @override
+  Future<void> onReady() async {
+    try {
+      Loader.show();
+      await Future.wait([
+        _findSupplierById(),
+        _findSupplierServices(),
+      ]);
+    } finally {
+      Loader.hide();
+    }
+  }
+
+  @action
+  Future<void> _findSupplierById() async {
+    try {
+      _supplierModel = await _supplierService.findById(_supplierId);
+    } catch (e, s) {
+      _log.error("error ao buscar dados do fornecedor", e, s);
+      Messages.alert("Error ao buscar doas do fornecedor");
+    }
+  }
+
+  @action
+  Future<void> _findSupplierServices() async {
+    try {
+      _supplierServices = await _supplierService.findServices(_supplierId);
+    } catch (e, s) {
+      _log.error("error ao buscar servicos do fornecedor", e, s);
+      Messages.alert("Erro ao buscar serevices do fornecedor");
+    }
+  }
+}
--- lib/app/module/supplier/supplier_module.dart
+++ lib/app/module/supplier/supplier_module.dart
@@ -1,16 +1,28 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../core/supplier/supplier_core_module.dart';
+import 'supplier_controller.dart';
 import 'supplier_page.dart';
 
 class SupplierModule extends Module {
   @override
-  void binds(i) {}
+  void binds(i) {
+    i.addLazySingleton(SupplierController.new);
+  }
+
+  @override
+  List<Module> get imports => [
+        // CoreModule(),
+        SupplierCoreModule(),
+      ];
 
   @override
   void routes(r) {
     r.child(
       Modular.initialRoute,
-      child: (context) => const SupplierPage(),
+      child: (context) => SupplierPage(
+        supplierId: r.args.data,
+      ),
     );
   }
 }
